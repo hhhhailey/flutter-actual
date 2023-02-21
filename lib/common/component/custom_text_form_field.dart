6e494bea0683e6b5e9ba93f97bb3f09133754567
hailey <gmlthd0908@gmail.com>
2023-02-21 10:59:58 +0900

    feat: Input Widget added

--- lib/common/component/custom_text_form_field.dart
+++ lib/common/component/custom_text_form_field.dart
@@ -2,13 +2,45 @@ import 'package:actual/common/const/colors.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({Key? key}) : super(key: key);
+  final bool obscureText;
+  final bool autofocus;
+  final String? hintText;
+  final String? errorText;
+  final ValueChanged<String> onChange;
 
+  const CustomTextFormField(
+      {required this.onChange,
+      this.obscureText = false,
+      this.autofocus = false,
+      this.hintText,
+      this.errorText,
+      Key? key})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
+    OutlineInputBorder baseBorder = const OutlineInputBorder(
+      borderSide: BorderSide(color: INPUT_BORDER_COLOR, width: 1.0),
+    );
+
     return TextFormField(
       cursorColor: PRIMARY_COLOR,
-      decoration: InputDecoration(contentPadding: EdgeInsets.all(20)),
+      obscureText: obscureText, // 비밀번호 작성할 때 안보이게 해줌
+      autofocus: autofocus, // true: 자동 포커스 활성화, false: 자동 포커스 해제
+      onChanged: onChange,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(color: BODY_TEXT_COLOR, fontSize: 14.0),
+        fillColor: INPUT_BG_COLOR,
+        filled: true, // true: 배경색 효과 있음, false: 배경색 효과 없음
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,27 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+  changeText(value) {
+    return print(value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            CustomTextFormField(),
+            CustomTextFormField(
+              onChange: (String value) => changeText(value),
+              hintText: '이메일을 입력해주세요',
+            ),
+            CustomTextFormField(
+              obscureText: true,
+              onChange: (String value) => changeText(value),
+              hintText: '비밀번호를 입력해주세요',
+            ),
           ],
         ),
       ),
